f144a38507c360788fd094957c4444224c5a049a
masamitusmauo <133548941+masamitusmauo@users.noreply.github.com>
2023-05-17 11:20:40 +0900

    Delete pages directory

--- pages/page_1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-code = '''
-import streamlit as st
-'''
-st.code(code, 'python')
--- pages/page_4.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-#データ分析
-df=pd.read_csv('./data/data2.csv',index_col='日付')
-st.line_chart(df)
-st.bar_chart(df)
